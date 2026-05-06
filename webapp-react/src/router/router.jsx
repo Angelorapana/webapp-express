21978c1a9140cfb2b7d47388cda8c76666b404bb
Angelo Rapanà <angelorapana@icloud.com>
2026-05-06 13:21:40 +0200

    Fix router

---
+++ webapp-react/src/router/router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom"
+
+import LayoutPrincipale from "../layouts/LayoutPrincipale"
+
+import HomePage from "../pages/HomePage"
+import PaginaDettaglioFilm from "../pages/PaginaDettaglioFilm"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutPrincipale />,
+
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      },
+
+      {
+        path: "/movies/:id",
+        element: <PaginaDettaglioFilm />
+      }
+    ]
+  }
+])
+
+export default router
