c18f25278f15313283b5aa124cee5fd272f852dd
Angelo Rapanà <angelorapana@icloud.com>
2026-05-06 13:59:49 +0200

    up homePage

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.static("public"));
 require("./db/collegamento");
 
 app.use(express.json());
+app.use(cors())
 
 const moviesRoutes = require("./routes/movieRoutes");
 app.use("/movies", moviesRoutes);
--- webapp-react/src/pages/homePage.jsx
+++ webapp-react/src/pages/homePage.jsx
@@ -1,7 +1,38 @@
+import axios from "axios"
+import { useEffect, useState  } from "react"
+import { Link } from "react-router-dom"
+
 function HomePage() {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+
+  axios.get("http://localhost:3000/movies")
+  .then((res) => {
+    console.log(res.data)
+    setMovies(res.data)
+  })
+
+}, [])
+
   return (
     <>
-      <h1>HomePage WebApp Film</h1>
+      <h1>HomePage</h1>
+      <ul>
+
+  {movies.map((movie) => (
+
+    <li key={movie.id}>
+
+  <Link to={`/movies/${movie.id}`}>
+    {movie.title}
+  </Link>
+
+</li>
+
+  ))}
+
+</ul>
     </>
   )
 }
