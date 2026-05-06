1e27483f7a2f0cd7086aba894b9bd27252daa726
Angelo Rapanà <angelorapana@icloud.com>
2026-05-06 10:31:55 +0200

    Creazione component Header

---
+++ webapp-react/src/components/header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-dark text-white p-3">
+      <div className="container">
+        <h2>La mia WebApp Film!</h2>
+      </div>
+    </header>
+  )
+}
+
+export default Header
